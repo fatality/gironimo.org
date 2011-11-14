a1f4dcca8059b4afb3644b7473f0695df0fb2c17
Marc Rochow <contact@gironimo.org>
2011-11-14 15:46:38 +0100

    removed typo errors in admin templates

--- web/templates/admin/blog/entry/markitup.js
+++ web/templates/admin/blog/entry/markitup.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
-  $("#id_content").markItUp(mySettings);
+    $("#id_content").markItUp(mySettings);
 });
+
--- web/templates/admin/blog/entry/wymeditor.js
+++ web/templates/admin/blog/entry/wymeditor.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
-  $("#id_content").wymeditor({
-	  skin: "django", lang: "{{ LANGUAGE_CODE }}",
-	  stylesheet: "{{ STATIC_URL }}zinnia/css/wymeditor_styles.css",
-	  updateSelector: "input:submit", updateEvent: "click",
-	  postInit: function(wym) {
-	      wym.hovertools();
-	  }
-      });
+    $("#id_content").wymeditor({
+        skin: "django",
+        lang: "{{ LANGUAGE_CODE }}",
+        stylesheet: "{{ STATIC_URL }}css/wymeditor_styles.css",
+        updateSelector: "input:submit", updateEvent: "click",
+        postInit: function(wym) {
+            wym.hovertools();
+        }
     });
+});
+
