c11d5c54211aa2116bf4fd3739146133c5f561c2
Marc Rochow <contact@gironimo.org>
2011-12-11 22:31:59 +0100

    moving blog urls to root urls

--- web/gironimo/views.py
+++
@@ -1,8 +0,0 @@
-from django.template import RequestContext
-from django.shortcuts import *
-
-
-def index(request):
-    return render_to_response('index.html', {
-    }, context_instance=RequestContext(request))
-
